288534b12b0b5fe2656c7a7bc4c998b4cfa9a6ef
Julio Hippler <julio.hippler@gmail.com>
2018-07-18 15:37:54 -0300

    MENUS
    Adicionados os menus, mas ainda faltam colocar os conteúdos dos mesmos.

--- www/wordpress/wp-config-sample.php
+++ www/wordpress/wp-config-sample.php
@@ -18,16 +18,15 @@
  * @package WordPress
  */
 
-// ** Configurações do MySQL - Você pode pegar estas informações
-// com o serviço de hospedagem ** //
+// ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'nome_do_banco_de_dados_aqui');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'nome_de_usuario_aqui');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'senha_aqui');
 
 /** Nome do host do MySQL */
 define('DB_HOST', 'localhost');
